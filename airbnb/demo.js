412c86f95908ddbe13f9045e517b2fcd8ab15b5c
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2023-01-25 20:04:37 -0800

    crud operations

--- airbnb/demo.js
+++ airbnb/demo.js
@@ -9,11 +9,27 @@ async function main() {
   try {
     await client.connect();
 
-    await findListingsWithMinimunBedroomsBathroomsAndMostRecentReviews(client, {
-      minimumNumberOfBedrooms: 4,
-      minimumNumberOfBathrooms: 2,
-      maximumNumberOfResults: 5,
-    });
+    await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
+    //----------
+    //await deleteListingByName(client, "Ribeira Charming Duplex");
+    //----------
+    // await updateAllListingsToHavePropertyType(client);
+    //----------
+    // await upsertListingByName(client, "Cozu Cottage", {
+    //   bedrooms: 2,
+    //   bathrooms: 2,
+    // });
+    //----------
+    // await updateListingByName(client, "Ribeira Charming Duplex", {
+    //   bedrooms: 6,
+    //   beds: 8,
+    // });
+    //----------
+    // await findListingsWithMinimunBedroomsBathroomsAndMostRecentReviews(client, {
+    //   minimumNumberOfBedrooms: 4,
+    //   minimumNumberOfBathrooms: 2,
+    //   maximumNumberOfResults: 5,
+    // });
   } catch (error) {
     console.error(e);
   } finally {
@@ -23,6 +39,80 @@ async function main() {
 
 main().catch(console.error);
 
+//DeleteMany
+
+async function deleteListingsScrapedBeforeDate(client, date) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .deleteMany({ last_scraped: { $lt: date } });
+
+  console.log(`${result.deletedCount} document(s) was/were deleted`);
+}
+
+// DeleteOne
+async function deleteListingByName(client, nameOfListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .deleteOne({ name: nameOfListing });
+
+  console.log(`${result.deletedCount} document(s) was/were deleted`);
+}
+
+// UpdateMany
+async function updateAllListingsToHavePropertyType(client) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .updateMany(
+      {
+        property_type: {
+          $exists: false,
+        },
+      },
+      {
+        $set: { property_type: "Unknown" },
+      }
+    );
+
+  console.log(`${result.matchedCount} document(s) matched the query criteria`);
+  console.log(`${result.modifiedCount} document(s) was/were updated`);
+}
+
+//Upsert listing by name
+
+async function upsertListingByName(client, nameOfListing, updateListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .updateOne(
+      { name: nameOfListing },
+      { $set: updateListing },
+      { upsert: true }
+    );
+
+  console.log(`${result.matchedCount} document(s) matched the query criteria`);
+  if (result.upsertedCount > 0) {
+    console.log(`One document was inserted with the id ${result.upsertedId}`);
+  } else {
+    console.log(`${result.modifiedCount} document(s) was/were updated`);
+  }
+}
+
+//Update Listing
+
+async function updateListingByName(client, nameOfListing, updateListing) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .updateOne({ name: nameOfListing }, { $set: updateListing });
+
+  console.log(`${result.matchedCount} document(s) matched the query criteria`);
+  console.log(`${result.modifiedCount} document was/where updated`);
+}
+
+// Listing multiple elements
 async function findListingsWithMinimunBedroomsBathroomsAndMostRecentReviews(
   client,
   {
